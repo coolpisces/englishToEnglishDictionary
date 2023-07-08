a49d834b34cc5b6bc0f6050d28d2d3eee3955765
coolpisces <coolpisces22@gmail.com>
2023-07-08 16:48:04 +0300

    finished

--- myScript.js
+++ myScript.js
@@ -6,25 +6,23 @@ const meaning = document.getElementById('meaning');
 const audio = document.getElementById('audio');
 const error = document.querySelector('.error');
 
+
 async function fetchApi() {
 
-    error.style.display = 'none'
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`;
     const result = await fetch(url).then(res => res.json());
-    console.log(result)
-    if (result.message) {
-        error.style.display = 'block';
+    console.log(result);
+    if (result.title) {
         translatedDiv.style.display = 'none';
-        error.textContent = result.message;
+        error.style.display = 'block';
+        error.textContent = result.title;
     } else {
-        translatedDiv.style.display = 'block';
         title.textContent = result[0].word;
         meaning.textContent = result[0].meanings[0].definitions[0].definition;
         audio.src = result[0].phonetics[0].audio;
-
+        translatedDiv.style.display = 'block'
+        error.style.display = 'none';
     }
 }
-
-
 
 button.addEventListener('click', fetchApi);
